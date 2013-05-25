568f88a341c3ee05df0d893dd17fd24ee0ac0d9d
Tom De Vylder <tom@penumbra.be>
2013-05-25 13:57:48 +0200

    Add missing parameter: hasrestart

--- manifests/service.pp
+++ manifests/service.pp
@@ -2,10 +2,11 @@
 #
 class postfwd::service {
   service { $::postfwd::service_name:
-    ensure    => $::postfwd::service_ensure,
-    enable    => $::postfwd::service_enable,
-    hasstatus => $::postfwd::service_hasstatus,
-    require   => Class['::postfwd::config'],
+    ensure     => $::postfwd::service_ensure,
+    enable     => $::postfwd::service_enable,
+    hasrestart => $::postfwd::service_hasrestart,
+    hasstatus  => $::postfwd::service_hasstatus,
+    require    => Class['::postfwd::config'],
   }
 }
 
